d13956234d8e6eca233b7fdd88b61d5708e4cc18
Unknown <fifaclubfootball2014@gmail.com>
2018-11-28 00:20:02 +0700

    Next Update

---
+++ Testgit/Testgit/Areas/Admin/Controllers/ThanhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Testgit.Areas.Admin.Controllers
+{
+    public class ThanhController : Controller
+    {
+        // GET: Admin/Thanh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
