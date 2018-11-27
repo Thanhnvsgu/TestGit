513b9be5ee09d45fedbe420809d25c9f29238743
minhphuc605 <43328859+minhphuc605@users.noreply.github.com>
2018-11-28 00:06:06 +0700

    New Update

---
+++ Testgit/Testgit/Areas/Guest/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Testgit.Areas.Guest.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Guest/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
